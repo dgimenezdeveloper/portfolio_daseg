9e10dd5a8b510b841d66b791d6463e6d8d179d50
Dario Gimenez <dgimenez.developer@gmail.com>
2023-04-15 07:07:43 -0300

    Inicio repositorio local

---
+++ js/desplazar-arriba.js
@@ -0,0 +1,9 @@
+const desplazarArriba = document.getElementById('desplazarse-hacia-arriba');
+
+desplazarArriba.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left:0,
+        behavior:"smooth"
+    });
+});
